32b0bb7c7d372a02a1c3e69f72ea66e51fb09453
Sc41ar <mrniki2396@gmail.com>
2022-11-19 21:55:45 +0700

    5 sdelal

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,22 @@ import java.io.UnsupportedEncodingException;
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException
     {
-        int bookCount = 0, magazineCount = 0;
+        int collectionCount = 0, magazineCount = 0, bookCount = 0;
         Scanner scanned = new Scanner(System.in);
-        System.out.print("Введите количество книг в библиотеке: ");
-        bookCount = scanned.nextInt();
-        Book[] bookArray = new Book[bookCount];
-        for (int i = 0; i < bookCount; i++)
-        {
-            bookArray[i] = new Book();
-            bookArray[i].Input();
+        System.out.print("Введите количество сборников в библиотеке: ");
+        collectionCount = scanned.nextInt();
+        Book[][] bookArray = new Book[collectionCount][];
+        for (int i = 0; i < collectionCount; i++) {
+            System.out.println("Ввеите количество книг в сборнике");
+            bookCount = scanned.nextInt();
+            bookArray[i] = new Book[bookCount + 1];
+            System.out.println("Введите информацию о самом сборнике");
+            bookArray[i][0] = new Book();
+            bookArray[i][0].Input();
+            for (int j = 1; j < bookCount + 1; j++) {
+                bookArray[i][j] = new Book();
+                bookArray[i][j].Input();
+            }
         }
         System.out.println("Введите количество журналов в библиотеке: ");
         magazineCount = scanned.nextInt();
@@ -23,10 +30,13 @@ public class Main {
             magazineArray[i].Input();
         }
 
-        for (int i = 0; i < bookCount; i++)
-        {
+        for (int i = 0; i < collectionCount; i++) {
             System.out.print("\n\n");
-            bookArray[i].Output();
+            System.out.println("Информация о сборнике");
+            bookArray[i][0].Output();
+            for (int j = 1; j < bookCount + 1; j++) {
+                bookArray[i][j].Output();
+            }
         }
 
         for (int i = 0; i < magazineCount; i++)
